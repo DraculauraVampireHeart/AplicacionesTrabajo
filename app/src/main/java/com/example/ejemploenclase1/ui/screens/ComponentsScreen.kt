81be833aacc92edbee31f854ca1bca3f0c9274da
DraculauraVampireHeart <ferellopez123@gmail.com>
2024-09-26 07:55:07 -0600

    Nuevos componentes de botones y filterchip

--- app/src/main/java/com/example/ejemploenclase1/ui/screens/ComponentsScreen.kt
+++ app/src/main/java/com/example/ejemploenclase1/ui/screens/ComponentsScreen.kt
@@ -1,13 +1,32 @@
 package com.example.ejemploenclase1.ui.screens
 
+import androidx.compose.foundation.layout.Arrangement
 import androidx.compose.foundation.layout.Column
+import androidx.compose.foundation.layout.fillMaxSize
 import androidx.compose.foundation.layout.padding
+import androidx.compose.foundation.layout.size
+import androidx.compose.material.icons.Icons
+import androidx.compose.material.icons.filled.FavoriteBorder
+import androidx.compose.material.icons.filled.Star
+import androidx.compose.material3.AssistChip
+import androidx.compose.material3.AssistChipDefaults
+import androidx.compose.material3.Button
 import androidx.compose.material3.DrawerValue
+import androidx.compose.material3.ElevatedButton
+import androidx.compose.material3.ExtendedFloatingActionButton
+import androidx.compose.material3.FilledTonalButton
+import androidx.compose.material3.FilterChip
+import androidx.compose.material3.FloatingActionButton
 import androidx.compose.material3.HorizontalDivider
+import androidx.compose.material3.Icon
+import androidx.compose.material3.LargeFloatingActionButton
 import androidx.compose.material3.ModalDrawerSheet
 import androidx.compose.material3.ModalNavigationDrawer
 import androidx.compose.material3.NavigationDrawerItem
+import androidx.compose.material3.OutlinedButton
+import androidx.compose.material3.SmallFloatingActionButton
 import androidx.compose.material3.Text
+import androidx.compose.material3.TextButton
 import androidx.compose.material3.rememberDrawerState
 import androidx.compose.runtime.Composable
 import androidx.compose.runtime.getValue
@@ -15,10 +34,14 @@ import androidx.compose.runtime.mutableStateOf
 import androidx.compose.runtime.remember
 import androidx.compose.runtime.rememberCoroutineScope
 import androidx.compose.runtime.setValue
+import androidx.compose.ui.Alignment
 import androidx.compose.ui.Modifier
+import androidx.compose.ui.tooling.preview.Preview
 import androidx.compose.ui.unit.dp
 import androidx.navigation.NavController
 import kotlinx.coroutines.launch
+import org.intellij.lang.annotations.JdkConstants.HorizontalAlignment
+
 
 @Composable
 fun ComponentScreen(navController: NavController){
@@ -56,6 +79,7 @@ fun ComponentScreen(navController: NavController){
                         }
                     }
                 )
+
             }
         }
     ) {
@@ -83,3 +107,94 @@ fun Content1(){
 fun Content2(){
     Text(text = "Content 2")
 }
+
+@Composable
+fun Buttons(){
+    Column (
+        horizontalAlignment = Alignment.CenterHorizontally,
+        verticalArrangement = Arrangement.SpaceEvenly,
+        modifier = Modifier
+            .fillMaxSize()
+    ){
+        Button(onClick = { }) {
+            Text(text = "Filled")
+        }
+        FilledTonalButton(onClick = { }) {
+            Text(text = "Tonal")
+        }
+        OutlinedButton(onClick = { }) {
+            Text(text = "Outlined")
+        }
+        ElevatedButton(onClick = { }) {
+            Text(text = "Outlined")
+        }
+        TextButton(onClick = { }) {
+            Text(text = "Outlined")
+        }
+    }
+        
+}
+
+//@Preview
+@Composable
+fun FloatingButtons() {
+    Column(
+        horizontalAlignment = Alignment.CenterHorizontally,
+        verticalArrangement = Arrangement.SpaceEvenly,
+        modifier = Modifier
+            .fillMaxSize()
+    ) {
+        FloatingActionButton(onClick = {}) {
+            Icon(Icons.Filled.FavoriteBorder, "")
+        }
+        SmallFloatingActionButton(onClick = {}) {
+            Icon(Icons.Filled.FavoriteBorder, "")
+        }
+        LargeFloatingActionButton(onClick = {}) {
+            Icon(Icons.Filled.FavoriteBorder, "")
+        }
+        ExtendedFloatingActionButton(
+            onClick = {},
+            icon = { Icon(Icons.Filled.FavoriteBorder, "")},
+            text = { Text(text = "Extended FAB")}
+        )
+    }
+}
+
+//@Preview
+@Composable
+fun Chips() {
+    Column(
+        horizontalAlignment = Alignment.CenterHorizontally,
+        verticalArrangement = Arrangement.SpaceEvenly,
+        modifier = Modifier
+            .fillMaxSize()
+    ) {
+        AssistChip(
+            onClick = {},
+            label = { Text(text = "Assist chip")},
+            leadingIcon = {
+                Icon(Icons.Filled.Star, "",
+                    modifier = Modifier.size(AssistChipDefaults.IconSize))
+            }
+        )
+        var selected by remember { mutableStateOf(false)}
+        FilterChip(
+            selected = selected,
+            onClick = {},
+            label = { Text(text = "Filter Chip")},
+            leadingIcon = if (selected){
+                {
+                    Icon(
+                        Icons.Filled.Star, "",
+                        modifier = Modifier.size(AssistChipDefaults.IconSize)
+                    )
+                }
+            }else{
+                null
+            }
+        )
+
+    }
+}
+
